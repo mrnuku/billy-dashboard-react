34e7334034594a3b3d2855106d23ed181a2262d6
mrnuku <mrnuku@gmail.com>
2022-11-19 06:56:30 +0100

    change menu to mui

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -3,23 +3,54 @@ import {
   Link,
   Outlet
 } from "react-router-dom";
+import Paper from '@mui/material/Paper';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import { createTheme, ThemeProvider } from "@mui/material";
 
 class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.theme = createTheme({
+      palette: {
+        primary: {
+          main: '#8879B0',
+        },
+        secondary: {
+          main: '#FBA1A1',
+        },
+      },
+    });
+  }
+
+
   render() {
     return (
-      <div style={{ display: "flex" }}>
-        <div style={{ padding: "10px", width: "100px", background: "#f0f0f0" }}>
-          <ul style={{ listStyleType: "none", padding: 0 }}>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/editdata">EditData</Link>
-            </li>
-          </ul>
+      <ThemeProvider theme={this.theme}>
+        <div style={{ display: "flex" }}>
+          <div style={{ padding: "10px" }}>
+            <Paper sx={{ width: 120, maxWidth: '100%' }}>
+              <MenuList>
+                <Link to="/">
+                  <MenuItem>
+                    <ListItemText>Dashboard</ListItemText>
+                  </MenuItem>
+                </Link>
+                <Divider />
+                <Link to="/editdata">
+                  <MenuItem>
+                    <ListItemText>Edit Data</ListItemText>
+                  </MenuItem>
+                </Link>
+              </MenuList>
+            </Paper>
+          </div>
+          <Outlet />
         </div>
-        <Outlet />
-      </div>
+      </ThemeProvider>
     )
   }
 }
