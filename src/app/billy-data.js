637b59b4e03a0fa80cf0aa347b2efb50d6e886d7
mrnuku <mrnuku@gmail.com>
2022-11-21 14:06:02 +0100

    add more test data

--- src/app/billy-data.js
+++ src/app/billy-data.js
@@ -39,6 +39,10 @@ export const billyDataSlice = createSlice({
 
     rows: [
       { id: 1, date: '2022-11-07T20:00:00Z', temperature: 5.4, naturalGas: 4628.6, electricity: 62369 },
+      { id: 2, date: '2022-11-08T21:45:00Z', temperature: 8.7, naturalGas: 4639.4, electricity: 62376 },
+      { id: 3, date: '2022-11-09T21:54:00Z', temperature: 9.2, naturalGas: 4645.9, electricity: 62385 },
+      { id: 4, date: '2022-11-10T21:40:00Z', temperature: 7, naturalGas: 4650.1, electricity: 62391 },
+      { id: 5, date: '2022-11-11T22:50:00Z', temperature: 6, naturalGas: 4655.8, electricity: 62397 },
     ],
   },
 
